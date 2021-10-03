6da5beb204fed8058092285ae6994309e33d9772
Rishit Vaish <rishitvaish@Rishits-MacBook-Pro.local>
2021-10-03 19:35:44 +0530

    Editors added,preview functionality added

--- pages/web.js
+++ pages/web.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import SplitPane from 'react-split-pane'
+import dynamic from 'next/dynamic'
+import styles from '../styles/web.module.css'
+const Editor = dynamic(import('./components/editor'), {ssr:false})
 
 function WebCompiler(){
+
+    const [height, setHeight] = useState('485px')
+    const [html, setHTML] = useState('')
+    const [css, setCSS] = useState('')
+    const [js, setJS] = useState('')
+    const [output, setOutput] = useState('')
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setOutput(`
+                <html>
+                    <head><style>${css}</style></head>
+                    <body>${html}</body>
+                    <script>${js}</script>
+                </html>
+            `)
+        }, 500)
+        return () => {clearTimeout(timer)}
+    }, [html, css, js])
+
     return(
-        <SplitPane split="horizontal" minSize={"50%"}>
+        <SplitPane split="horizontal" minSize={"50%"} onDragFinished={ (height) => {setHeight(`${height - 40}px`)} }>
             <SplitPane split="vertical" minSize={"33%"}>
-                <div>1</div>
+                <Editor title="HTML" mode="html" value={html} onChange={setHTML} height={height} />
                 <SplitPane split="vertical" minSize={"50%"}>
-                    <div>2</div>
-                    <div>3</div>
+                    <Editor title="CSS" mode="css" value={css} onChange={setCSS} height={height} />
+                    <Editor title="JS" mode="javascript" value={js} onChange={setJS} height={height} />
                 </SplitPane>
             </SplitPane>
-            <div>4</div>
+            <iframe className={styles.preview} srcDoc={output} />
         </SplitPane>
     )
 }
