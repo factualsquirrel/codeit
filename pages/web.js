89b5d5044b2ec0ed2197364db6c178c6e477f663
Rishit Vaish <rishitvaish@Rishits-MacBook-Pro.local>
2021-10-03 18:47:01 +0530

    Web Runner Layout

--- pages/web.js
+++ pages/web.js
@@ -1,3 +1,18 @@
+import React from 'react'
+import SplitPane from 'react-split-pane'
+
 function WebCompiler(){
-    return <div>abc</div>;
+    return(
+        <SplitPane split="horizontal" minSize={"50%"}>
+            <SplitPane split="vertical" minSize={"33%"}>
+                <div>1</div>
+                <SplitPane split="vertical" minSize={"50%"}>
+                    <div>2</div>
+                    <div>3</div>
+                </SplitPane>
+            </SplitPane>
+            <div>4</div>
+        </SplitPane>
+    )
 }
+export default WebCompiler
