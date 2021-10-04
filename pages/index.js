c92cbc060f9dfc7574e52d6905abc74c4da02809
Jasraj S. Juneja <factualsquirrel@gmail.com>
2021-10-05 00:06:48 +0530

    Made index layout

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
-export default function home(){
-	return <div>abc</div>;
+import React from 'react'
+import styles from '../styles/index.module.css'
+
+function Home() {
+    return (
+        <>
+        <div className={`${styles.split} ${styles.left}`}>
+
+        </div>
+        <div className={`${styles.split} ${styles.right}`}>
+
+        </div>
+        </>
+    )
 }
+
+export default Home
